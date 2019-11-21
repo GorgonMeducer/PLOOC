f525b60346f72acf63af524e976163f481f04ebe
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2019-11-21 13:48:01 +0000

    Fix Typo

--- example/trace/trace.c
+++ example/trace/trace.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
- *   Copyrig;ht(C)2009-2019 by GorgonMeducer<embedded_zhuoran@hotmail.com>    *
+ *   Copyright(C)2009-2019 by GorgonMeducer<embedded_zhuoran@hotmail.com>    *
  *                                                                           *
  *  Licensed under the Apache License, Version 2.0 (the "License");          *
  *  you may not use this file except in compliance with the License.         *
